ac2addd62164fb8c74c6171afda6fa284c5c467b
Anthony Njuguna <tonju2@yahoo.com>
2021-02-07 16:31:06 -0600

    2nd version

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "12d406a0fb4568cd4a802f8d87af5ff6"
+weather_api_key = "711342c89d668686a1d7b55da5ed939c"
 
 # Google API Key
-g_key = "AIzaSyD6t6z8dCzCclgBdALFiTidUgiMSM3dbxc"
+g_key = "AIzaSyC4k2x9qCodW_HUzAvwUMMber_C7jNJOGA"
